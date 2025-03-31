ff55cc08731f1a99ed3484ddc354661e0bc25acd
pavithranluffy <84136495+Pavithranluffy@users.noreply.github.com>
2025-03-31 18:28:15 +0000

    caching

--- src/App.js
+++ src/App.js
@@ -6,37 +6,41 @@ export default function App() {
   //First To store the input let us create a state
   const [input, setInput] = useState("");
   const [results, setResults] = useState([]);
-  const [showResult , setShowresults]= useState(false);
+  const [showResult, setShowresults] = useState(false);
+
+  //To handle the cache
+  const [cache, setCache] = useState({});
   const fetchData = async () => {
+    if (cache[input]) {
+      console.log("From Cache", input);
+      setResults(cache[input]);
+      return; //after getting from cache jumming out of the function
+    }
+
     try {
-      console.log("Api call hit",input);
+      console.log("Api call hit", input);
       const response = await fetch(
         `https://dummyjson.com/recipes/search?q=${input}`
       );
       const data = await response.json();
       setResults(data.recipes);
+      setCache((prev) => ({ ...prev, [input]: data.recipes })); //Updating the cache not overwritting
     } catch (e) {
       console.log("The Error Occured", e);
     }
   };
 
   useEffect(() => {
-    if(input.length >0){
-
+    if (input.length > 0) {
       const timer = setTimeout(
+        fetchData,
 
-        fetchData
-        
-      , 300);
-      return ()=>{
-
-        clearTimeout(timer)
-    
+        300
+      );
+      return () => {
+        clearTimeout(timer);
+      };
     }
-
-    }
-
-   
   }, [input]);
 
   return (
@@ -48,8 +52,8 @@ export default function App() {
           className="input-text"
           value={input}
           onChange={(e) => setInput(e.target.value)}
-          onFocus={()=> setShowresults(true)}
-          onBlur={()=>setShowresults(false)}
+          onFocus={() => setShowresults(true)}
+          onBlur={() => setShowresults(false)}
         ></input>
         {showResult && (
           <div className="result-container">
